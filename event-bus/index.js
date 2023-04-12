a11db68ad85f0d9733d06282ab8188bff1485ac7
Warren <warrengamedev@gmail.com>
2023-04-12 10:37:17 +0100

    created event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    // posts
+    axios.post('http://localhost:4000/events', event);
+    // comments
+    axios.post('http://localhost:4001/events', event);
+    // query service
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({ status: 'ok' });
+});
+
+app.listen(4005, () => {
+    console.log('Listening no 4005');
+})
