4613d0210c13678ce0fca58b8486782619bc1347
Warren <warrengamedev@gmail.com>
2023-04-12 16:57:56 +0100

    update rendering of comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,22 @@ import axios from 'axios';
 
 const CommentList = ({ comments }) => {
     const renderedComments = Object.values(comments).map(comment => {
-        return <li key={comment.id}>{comment.content}</li>;
+        let content;
+        switch (comment.status) {
+            case 'approved':
+                content = comment.content;
+                break;
+            case 'pending':
+                content = 'awaiting moderation';
+                break;
+            case 'rejected':
+                content = 'comment removed';
+                break;
+            default:
+                break;
+        }
+
+        return <li key={comment.id}>{content}</li>;
     });
 
 
