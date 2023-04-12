2cc1dbed4f0387c78aa1a9a9faa04ca08c7b97a0
Warren <warrengamedev@gmail.com>
2023-04-12 16:58:54 +0100

    query event bus and update
    if query service fails, it will check the event bus for events and handle them on restart

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,20 +5,28 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
+    events.push(event);
     // posts
-    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4000/events', event).catch(err => console.log("error bus caught error"));
     // comments
-    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4001/events', event).catch(err => console.log("error bus caught error"));
     // query service
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4002/events', event).catch(err => console.log("error bus caught error"));
     // moderation
     
-    axios.post('http://localhost:4003/events', event);
-
+    axios.post('http://localhost:4003/events', event).catch(err => console.log("error bus caught error"));
+    console.log("EVENT BUS created event, returning OK, events: ", events);
     res.send({ status: 'ok' });
+});
+
+app.get('/events', (req, res)=> {
+    console.log("EVENT-BUS sending all events: ", events);
+    res.send(events);
 });
 
 app.listen(4005, () => {
